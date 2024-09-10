3e95a1fea25a3d081fbe6f4d06e32f60d445979c
onfranciis <onukwuf@gmail.com>
2024-09-10 14:31:57 +0100

    feat: migrate to react

---
+++ src/data/Mock.ts
@@ -0,0 +1,29 @@
+export const Skills = [
+  {
+    title: "UI/UX Developer",
+  },
+  {
+    title: "Software Developer",
+  },
+];
+
+export const PreName = [
+  {
+    title: "Hello, I'm",
+  },
+  {
+    title: "Bonjour, J'mapelle",
+  },
+];
+
+export const Industries = [
+  {
+    title: "Legal",
+  },
+  {
+    title: "Finance",
+  },
+  {
+    title: "Healthcare",
+  },
+];
