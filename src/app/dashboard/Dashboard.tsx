e400216c2b8164b9269f92d6ad5f532528d7ef80
onfranciis <onukwuf@gmail.com>
2024-09-28 15:23:37 +0100

    feat: create ProCard

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import ProCard from "@/components/ProCard";
 import ProjectCards from "@/components/ProjectCards";
 import { Suspense } from "react";
 
@@ -7,12 +8,7 @@ const Dashboard = () => {
       <div className="DashboardMain">
         <p className="intro">Your portfolio</p>
 
-        <div className="pro">
-          <img alt="" src="/icon/shapes.svg" width={20} height={20} />
-          <p>You can create a sub-portfolio account under your profile</p>
-
-          <button>Upgrade to Pro</button>
-        </div>
+        <ProCard />
 
         <div className="cards">
           {Portfolios.map((portfolio) => (
---
+++ src/components/ProCard.tsx
@@ -0,0 +1,12 @@
+const ProCard = () => {
+  return (
+    <div className="ProCard">
+      <img alt="" src="/icon/shapes.svg" width={20} height={20} />
+      <p>You can create a sub-portfolio account under your profile</p>
+
+      <button>Upgrade to Pro</button>
+    </div>
+  );
+};
+
+export default ProCard;
