96ff5b262256e340ffb3ed454222543eac46677a
onfranciis <onukwuf@gmail.com>
2024-10-01 20:49:39 +0100

    feat: create Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,16 @@
+const Loader = () => {
+  return (
+    <div className="Loader">
+      <img
+        alt="Portixel Logo"
+        src="/icon/logo.svg"
+        width={43}
+        height={43}
+        className="logo"
+      />
+      <p>Wait for a moment while we get your portfolio ready...</p>
+    </div>
+  );
+};
+
+export default Loader;
