c73543bc5d95cddb7a89e0981249418694557c32
onfranciis <onukwuf@gmail.com>
2024-10-02 02:28:38 +0100

    feat: add ProjectModal

--- src/components/ProjectCards2.tsx
+++ src/components/ProjectCards2.tsx
@@ -1,4 +1,5 @@
-import { memo } from "react";
+import ProjectModal from "@/components/ProjectModal";
+import { memo, useState } from "react";
 
 interface IProjectCards {
   Data: {
@@ -13,20 +14,32 @@ interface IProjectCards {
 const ProjectCards = ({
   Data: { bgImg, title, size, date },
 }: IProjectCards) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div
-      className="ProjectCard2"
-      style={{ backgroundImage: `url("${bgImg}")` }}
-    >
-      <div className="bottom">
-        <p className="file">{title}</p>
-        <p className="size">{size}</p>
-        <p className="date">{date}</p>
-        <button className="more">
-          <img alt="" src="/icon/more.svg" width={34} height={20} />
-        </button>
+    <>
+      <div
+        className={`ProjectCard2 ${toggleMenu && "selected"}`}
+        style={{ backgroundImage: `url("${bgImg}")` }}
+      >
+        <div className="bottom">
+          <p className="file">{title}</p>
+          <p className="size">{size}</p>
+          <p className="date">{date}</p>
+
+          <button
+            className="more"
+            onClick={() => {
+              setToggleMenu(true);
+            }}
+          >
+            <img alt="" src="/icon/more.svg" width={34} height={20} />
+          </button>
+        </div>
       </div>
-    </div>
+
+      {toggleMenu && <ProjectModal toggleProjectModal={setToggleMenu} />}
+    </>
   );
 };
 
