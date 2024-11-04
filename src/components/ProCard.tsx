8a0dc7aa2a5c6d85e01b036346d9b886ea468a91
onfranciis <onukwuf@gmail.com>
2024-11-04 10:48:01 +0100

    fix: update ProCard

--- src/components/ProCard.tsx
+++ src/components/ProCard.tsx
@@ -1,8 +1,16 @@
-const ProCard = () => {
+interface IProCardProps {
+  img?: string;
+  title?: string;
+}
+
+const ProCard = ({
+  img = "/icon/shapes.svg",
+  title = "You can create a sub-portfolio account under your profile",
+}: IProCardProps) => {
   return (
     <div className="ProCard">
-      <img alt="" src="/icon/shapes.svg" width={20} height={20} />
-      <p>You can create a sub-portfolio account under your profile</p>
+      <img alt="" src={img} width={20} height={20} />
+      <p>{title}</p>
 
       <button>Upgrade to Pro</button>
     </div>
